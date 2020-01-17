c5882b4ea617b80547be2da7dfc6efcf51b6c88f
sarbjot-14 <sarbjot_14@hotmail.com>
2020-01-17 00:57:44 -0800

    fixed back button

--- lib/browzing.dart
+++ lib/browzing.dart
@@ -20,85 +20,93 @@ class _BrowzingState extends State<Browzing> {
   int credits = 0;
   List<Item> itemList = new Data().itemList;
   //final ref = FirebaseStorage.instance.ref().child('allbirds');
+  Future<bool> _onWillPop() {
+    Navigator.of(context).pop(credits.toString());
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: PreferredSize(
-          preferredSize: Size.fromHeight(80.0),
-          child: AppBar(
-            backgroundColor: Colors.teal[400],
-            leading: new Container(),
-            actions: <Widget>[
-              Center(child: Text("Checkout Credits")),
-              new IconButton(
-                icon: new Icon(Icons.shopping_cart),
-                onPressed: () => Navigator.of(context).pop(credits.toString()),
+    return WillPopScope(
+        onWillPop: _onWillPop,
+        child: new Scaffold(
+            appBar: PreferredSize(
+              preferredSize: Size.fromHeight(80.0),
+              child: AppBar(
+                backgroundColor: Colors.teal[400],
+                leading: new Container(),
+                actions: <Widget>[
+                  Center(child: Text("Checkout Credits")),
+                  new IconButton(
+                    icon: new Icon(Icons.shopping_cart),
+                    onPressed: () =>
+                        Navigator.of(context).pop(credits.toString()),
+                  ),
+                ],
+                title: Text("Browzing"),
+                flexibleSpace: FlexibleSpaceBar(
+                  title: Text("You have ${credits.toString()} credits"),
+                  titlePadding: EdgeInsets.fromLTRB(0, 65, 0, 5),
+                  centerTitle: true,
+                ),
               ),
-            ],
-            title: Text("Browzing"),
-            flexibleSpace: FlexibleSpaceBar(
-              title: Text("You have ${credits.toString()} credits"),
-              titlePadding: EdgeInsets.fromLTRB(0, 65, 0, 5),
-              centerTitle: true,
             ),
-          ),
-        ),
-        body: ListView.builder(
-            itemCount: itemList.length,
-            itemBuilder: (BuildContext ctxt, int index) {
-              return new VisibilityDetector(
-                key: Key(itemList[index].id),
-                onVisibilityChanged: (VisibilityInfo info) {
-                  //print('${info.visibleFraction} and ${itemList[index].id}');
-                  if (!itemList[index].seen) {
-                    if (info.visibleFraction > 0.4) {
-                      itemList[index].seenEnough++;
-                      if (itemList[index].seenEnough >= 3) {
-                        setState(() {
-                          itemList[index].seen = true;
-                          credits++;
-                          currentIndex = index + 1;
-                        });
+            body: ListView.builder(
+                itemCount: itemList.length,
+                itemBuilder: (BuildContext ctxt, int index) {
+                  return new VisibilityDetector(
+                    key: Key(itemList[index].id),
+                    onVisibilityChanged: (VisibilityInfo info) {
+                      //print('${info.visibleFraction} and ${itemList[index].id}');
+                      if (!itemList[index].seen) {
+                        if (info.visibleFraction > 0.4) {
+                          itemList[index].seenEnough++;
+                          if (itemList[index].seenEnough >= 3) {
+                            setState(() {
+                              itemList[index].seen = true;
+                              credits++;
+                              //currentIndex = index + 1;
+                            });
+                          }
+                        }
                       }
-                    }
-                  }
-                },
-                child: Container(
-                  padding: EdgeInsets.symmetric(vertical: 40, horizontal: 5),
-                  child: Card(
-                    color:
-                        itemList[index].seen ? Colors.teal[100] : Colors.white,
-                    elevation: 5,
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        ListTile(
-                          leading: Icon(Icons.album),
-                          title: Text(itemList[index].title.toString()),
-                          subtitle: Text(itemList[index].id),
-                        ),
-                        Container(
-                          child: ImageWidget(index: index + 1),
-                        ),
-                        ButtonBar(
+                    },
+                    child: Container(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 40, horizontal: 5),
+                      height: 600,
+                      child: Card(
+                        color: itemList[index].seen
+                            ? Colors.teal[50]
+                            : Colors.white,
+                        elevation: 5,
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
                           children: <Widget>[
-                            FlatButton(
-                              child: Text('BUY TICKETS'),
-                              onPressed: () {/* ... */},
+                            ListTile(
+                              leading: Icon(Icons.album),
+                              title: Text(itemList[index].title.toString()),
+                              subtitle: Text(itemList[index].id),
                             ),
-                            FlatButton(
-                              child: Text('LISTEN'),
-                              onPressed: () {/* ... */},
+                            Container(
+                              child: ImageWidget(index: index + 1),
+                            ),
+                            ButtonBar(
+                              children: <Widget>[
+                                FlatButton(
+                                  child: Text('BUY TICKETS'),
+                                  onPressed: () {/* ... */},
+                                ),
+                                FlatButton(
+                                  child: Text('LISTEN'),
+                                  onPressed: () {/* ... */},
+                                ),
+                              ],
                             ),
                           ],
                         ),
-                      ],
+                      ),
                     ),
-                  ),
-                ),
-              );
-              ;
-            }));
+                  );
+                })));
   }
 }
